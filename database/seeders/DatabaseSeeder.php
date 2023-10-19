70b7b929e948930f5c60147df576df82294a5349
glitchy <zschopper@gmail.com>
2023-10-19 15:32:32 +0200

    User, Book, Copy factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory(10)->create();
+        \App\Models\Book::factory(10)->create();
+        \App\Models\Copy::factory(10)->create();
     }
 }
